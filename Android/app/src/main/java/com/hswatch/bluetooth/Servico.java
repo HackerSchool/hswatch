74ba55ac5c3733edb0bbdf665513ace0056f5b32
Filipe Varela <43725698+filipe-varela@users.noreply.github.com>
2020-05-11 19:29:04 +0100

    Update Servico.java - added writting file and the file

--- Android/app/src/main/java/com/hswatch/bluetooth/Servico.java
+++ Android/app/src/main/java/com/hswatch/bluetooth/Servico.java
@@ -21,6 +21,7 @@ import com.hswatch.definicoes;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -287,6 +288,17 @@ public class Servico extends Service {
         }
     }
 
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("registos.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
     class ThreadConectado extends Thread{
 
         private final BluetoothSocket bluetoothSocket;
@@ -342,6 +354,7 @@ public class Servico extends Service {
                                 caracteres[bufferposition++] = (char) bytes;
                             }
                         }
+                        writeToFile(mensagemRecebida.toString(), getApplicationContext());
                         if (mensagemRecebida.toString().equals(INDICADOR_CLIMA)) {
                             List<String> mensagemTemperatura = profileDispositivo.jsonParserTempo();
                             if (mensagemTemperatura != null) {
